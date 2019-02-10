a9147c915a3bdd81a8d44f43d4c344cee2294fab
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-02-11 01:30:51 +0200

    building the contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+
+contract Lottery{
+    address public manager;
+    address[] public player;
+    
+    function Lottery() public {
+        manager = msg.sender;
+    }
+    
+    function enter() public  payable{
+        require(msg.value > 10 ether );
+        player.push(msg.sender);
+    }
+    
+    function random() private view returns (uint256){
+        return uint256(keccak256(block.difficulty, now, player));
+    }
+    
+    function pickWinner() public {
+        require(msg.sender == manager)
+        uint256 index = random() % player.length;
+        player[index].transfer(this.balance);
+        player = new address[](0);
+    }
+}
+
